d42a0a024fd244963ef249c1fedafb335156acf4
Sujit Akulwar <85801946+SujitAkulwar@users.noreply.github.com>
2022-08-10 19:23:25 +0530

    1

--- index.js
+++ index.js
@@ -1,66 +1,82 @@
 var string = ""; 
+var string2 = "";
 var ans;
 var counter=0;
+var counter2 = 0;
+var n,n2;
+
 function start(){
-    counter = 0;
-    string = "";
-    game();
-}
-
-function fun(i){
-    
-    if(i==string[-1]){
-        counter++;
-        $("h1").text(counter);
-        console.log(i);
-    }
-    else{
-        $("h1").text("score = "+counter);
-    }
-    game();
-}
-
-function game(){
     ans = Math.floor(Math.random()*4);
     string += ans;
     Play(ans);
 }
 
+function fun(i){
+    n = string.length;
+    n2 = string2.length; 
+    if(n == n2){
+        check();
+    }else{
+        string2 += i;
+        game();
+    }
+}
+
+function check(){
+    if(string != string2){
+        wrong_ans();
+    }
+    $("h1").text("score = "+counter);
+}
+
+function wrong_ans(){
+    string = "";
+    string2 = "";
+    counter = 0;
+    counter2 = 0;
+    $("#start").text("Restart");
+    var w_sound = document.createElement("audio");
+    w_sound.src="wrong.mp3";
+    w_sound.autoPlay=false;
+    w_sound.preLoad=true;       
+    w_sound.play();
+}
+
 function Play(ans){
     if(ans==0){
-        setTimeout(function(){ $(".red").css("background-color", "red");},500);
         var red_sound = document.createElement("audio");
         red_sound.src="red.mp3";
         red_sound.autoPlay=false;
         red_sound.preLoad=true;       
         red_sound.play();
+        setTimeout(function(){ $(".red").css("background-color", "red");},500);
         $(".red").css("background-color", "pink");
     }
     else if(ans==1){
-        setTimeout(function(){$(".yellow").css("background-color", "yellow");},500);
         var y_sound = document.createElement("audio");
         y_sound.src="yellow.mp3";
         y_sound.autoPlay=false;
         y_sound.preLoad=true;       
         y_sound.play();
+        setTimeout(function(){$(".yellow").css("background-color", "yellow");},500);
         $(".yellow").css("background-color", "gray");
     }
     else if(ans==2){
-        setTimeout(function(){ $(".green").css("background-color", "green");},500);
         var g_sound = document.createElement("audio");
         g_sound.src="green.mp3";
         g_sound.autoPlay=false;
         g_sound.preLoad=true;       
         g_sound.play();
+        setTimeout(function(){ $(".green").css("background-color", "green");},500);
         $(".green").css("background-color", "purple");
     }
     else{
-        setTimeout(function(){ $(".blue").css("background-color", "blue");},500);
         var b_sound = document.createElement("audio");
         b_sound.src="blue.mp3";
         b_sound.autoPlay=false;
         b_sound.preLoad=true;       
-        b_sound.play();
+        b_sound.play();    
+        setTimeout(function(){ $(".blue").css("background-color", "blue");},500);
         $(".blue").css("background-color", "skyblue");
     }
 }
