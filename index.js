70fb1ec0f4e67266273f59bcbf645347db369d44
Sujit Akulwar <85801946+SujitAkulwar@users.noreply.github.com>
2022-08-10 15:43:49 +0530

    2

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 var string = ""; 
 var ans;
+var counter=0;
 function start(){
+    counter = 0;
     string = "";
     game();
 }
 
 function fun(i){
-
+    
+    if(i==string[-1]){
+        counter++;
+        $("h1").text(counter);
+        console.log(i);
+    }
+    else{
+        $("h1").text("score = "+counter);
+    }
     game();
 }
 
@@ -18,36 +28,40 @@ function game(){
 
 function Play(ans){
     if(ans==0){
-        $(".red").css("background-color", "pink");
+        setTimeout(function(){ $(".red").css("background-color", "red");},500);
         var red_sound = document.createElement("audio");
         red_sound.src="red.mp3";
         red_sound.autoPlay=false;
         red_sound.preLoad=true;       
         red_sound.play();
+        $(".red").css("background-color", "pink");
     }
     else if(ans==1){
-        $(".yellow").css("background-color", "gray");
+        setTimeout(function(){$(".yellow").css("background-color", "yellow");},500);
         var y_sound = document.createElement("audio");
         y_sound.src="yellow.mp3";
         y_sound.autoPlay=false;
         y_sound.preLoad=true;       
         y_sound.play();
+        $(".yellow").css("background-color", "gray");
     }
     else if(ans==2){
-        $(".green").css("background-color", "purple");
+        setTimeout(function(){ $(".green").css("background-color", "green");},500);
         var g_sound = document.createElement("audio");
         g_sound.src="green.mp3";
         g_sound.autoPlay=false;
         g_sound.preLoad=true;       
         g_sound.play();
+        $(".green").css("background-color", "purple");
     }
     else{
-        $(".blue").css("background-color", "skyblue");
+        setTimeout(function(){ $(".blue").css("background-color", "blue");},500);
         var b_sound = document.createElement("audio");
         b_sound.src="blue.mp3";
         b_sound.autoPlay=false;
         b_sound.preLoad=true;       
         b_sound.play();
+        $(".blue").css("background-color", "skyblue");
     }
 }
 
